e0dd4855bf38d0061edaa727707f5e7d123ebde6
Ahmad Arif Faizin <faizin.arief@gmail.com>
2024-02-23 15:48:10 +0700

    duplicate ImageClassifierHelper

---
+++ ILT4/TensorFlow Lite/app/src/main/java/com/dicoding/picodiploma/mycamera/ObjectDetectorHelper.kt
@@ -0,0 +1,134 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.SystemClock
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Surface
+import androidx.camera.core.ImageProxy
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+
+class ObjectDetectorHelper(
+    var threshold: Float = 0.1f,
+    var maxResults: Int = 3,
+    val modelName: String = "mobilenet_v1.tflite",
+    val context: Context,
+    val classifierListener: ClassifierListener?
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+    private fun setupImageClassifier() {
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+        val baseOptionsBuilder = BaseOptions.builder()
+            .setNumThreads(4)
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classifyStaticImage(imageUri: Uri) {
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder().build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+        }.copy(Bitmap.Config.ARGB_8888, true)?.let { bitmap ->
+            val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+
+            val imageProcessingOptions = ImageProcessingOptions.builder()
+//                .setOrientation(getOrientationFromRotation(image.imageInfo.rotationDegrees))
+                .build()
+
+            var inferenceTime = SystemClock.uptimeMillis()
+            val results = imageClassifier?.classify(tensorImage, imageProcessingOptions)
+            inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+            classifierListener?.onResults(
+                results,
+                inferenceTime
+            )
+        }
+    }
+
+    fun classifyLiveImage(image: ImageProxy) {
+
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder()
+            .build()
+
+        val bitmapBuffer = Bitmap.createBitmap(
+            image.width,
+            image.height,
+            Bitmap.Config.ARGB_8888
+        )
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+        image.close()
+
+        val tensorImage: TensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmapBuffer))
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setOrientation(getOrientationFromRotation(image.imageInfo.rotationDegrees))
+            .build()
+
+        var inferenceTime = SystemClock.uptimeMillis()
+        val results = imageClassifier?.classify(tensorImage, imageProcessingOptions)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        classifierListener?.onResults(
+            results,
+            inferenceTime
+        )
+    }
+
+    private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
+        return when (rotation) {
+            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?,
+            inferenceTime: Long
+        )
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
+    }
+}
