756cde6be7ec52c56c6f55c01657e13bd58ca8cb
Arif <faizin.arief@gmail.com>
2024-03-31 10:15:33 +0700

    add @JvmOverload option and change init function

--- ILT5/Custom View/app/src/main/java/com/dicoding/picodiploma/mycustomview/MyButton.kt
+++ ILT5/Custom View/app/src/main/java/com/dicoding/picodiploma/mycustomview/MyButton.kt
@@ -8,24 +8,23 @@ import android.view.Gravity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 
-class MyButton : AppCompatButton {
+class MyButton
+    //Opsi selain definisi constructor satu per satu
+    //@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : AppCompatButton(context, attrs) {
+: AppCompatButton {
 
-    private lateinit var enabledBackground: Drawable
-    private lateinit var disabledBackground: Drawable
-
+    private var enabledBackground: Drawable
+    private var disabledBackground: Drawable
     private var txtColor: Int = 0
 
-    // Konstruktor dari MyButton
-    constructor(context: Context) : super(context) {
-        init()
-    }
+    // Konstruktor untuk inisialiasi MyButton
+    constructor(context: Context) : super(context) // untuk di Activity/Fragment
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)  // untuk di XML
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init()
+    init {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
     }
 
     // Metode onDraw() digunakan untuk mengcustom button ketika enable dan disable
@@ -47,10 +46,4 @@ class MyButton : AppCompatButton {
         text = if(isEnabled) "Submit" else "Isi Dulu"
     }
 
-    // pemanggilan Resource harus dilakukan saat kelas MyButton diinisialisasi, jadi harus dikeluarkan dari metode onDraw
-    private fun init() {
-        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
-        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
-        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
-    }
 }
